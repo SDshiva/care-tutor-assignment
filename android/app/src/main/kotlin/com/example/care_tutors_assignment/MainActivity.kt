a3448bdae745caddc392630c220ca8191715038d
Shibam Dey <shibam.dey.sd@gmail.com>
2024-09-11 18:14:51 +0600

    First Commit

---
+++ android/app/src/main/kotlin/com/example/care_tutors_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.care_tutors_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "CareTutorsApp",
+    ),
+  );
+}
